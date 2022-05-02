a3208075dbbf4ccdad5bdec0c5eeb3f565ecbf8b
Manish <2020ht66023@wilp.bits-pilani.ac.in>
2022-05-02 22:31:29 +0530

    first commit

---
+++ src/main/java/com/javatechie/spring/mongo/api/model/Products.java
@@ -0,0 +1,63 @@
+package com.javatechie.spring.mongo.api.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@ToString
+@Document(collection = "products")
+public class Products {
+	
+	@Id
+	private String id;
+	private String title;
+	private String brand;
+	private Integer price;
+	private String description;
+	private String image;
+
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public Integer getPrice() {
+		return price;
+	}
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	
+	
+	
+}
